74d2aca44d53d42b29771231736f0e4ce5fabb50
David Eadie <david@eadie.net>
2020-04-26 11:54:55 +0100

    Split unix and windows config for git
    Based on Alex's implemetation: https://github.com/alextercete/dotfiles/blob/master/.gitconfig

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 rm ~/.zshrc
 rm ~/.p10k.zsh
 rm ~/.gitconfig
+rm ~/.gitconfig.unix
 
 # Link files from the repo to the HOME dir
 ln -sv ~/.dotfiles/zsh/.zshrc ~
 ln -sv ~/.dotfiles/zsh/.p10k.zsh ~
 ln -sv ~/.dotfiles/git/.gitconfig ~
+ln -sv ~/.dotfiles/git/.gitconfig.unix ~
