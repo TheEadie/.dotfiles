b8dc9c511bd3542aa6aea7bd7a4a4e3a0153c7f4
David Eadie <david@eadie.net>
2020-05-01 10:35:24 +0100

    Add setup script for Windows
    The Windows Terminal directory propably needs more work as the path looks like it contains a guid

---
+++ install.ps1
@@ -0,0 +1,18 @@
+# Run as admin (Can only create links as Admin)
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit 
+}
+
+$CurrentDirectory = $PSScriptRoot
+$HomeDirectory = $env:UserProfile
+$WindowsTerminalConfigDirectory = Join-Path $env:LOCALAPPDATA "Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+
+
+Write-Output $CurrentDirectory
+
+# Git
+New-Item -ItemType SymbolicLink -Path (Join-Path $HomeDirectory ".gitconfig") -Target (Join-Path $CurrentDirectory "git/.gitconfig")
+New-Item -ItemType SymbolicLink -Path (Join-Path $HomeDirectory ".gitconfig.windows") -Target (Join-Path $CurrentDirectory "git/.gitconfig.windows")
+
+# Windows Terminal
+New-Item -ItemType SymbolicLink -Path (Join-Path $WindowsTerminalConfigDirectory "settings.json") -Target (Join-Path $CurrentDirectory "windows-terminal/settings.json")
