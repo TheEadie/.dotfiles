fbfa7113d9a5fa7a2c384cbe8c26fd0a3b8cbce4
David Eadie <david@eadie.net>
2022-07-30 21:36:52 +0100

    Track starship config in .dotfiles

--- install-fish.sh
+++ install-fish.sh
@@ -6,12 +6,14 @@ rm ~/.gitconfig
 rm ~/.gitconfig.unix
 rm ~/.gitconfig.redgate
 rm ~/.config/fish/config.fish
+rm ~/.config/starship.toml
 
 # Link files from the repo to the HOME dir
 ln -sv ~/.dotfiles/git/.gitconfig ~
 ln -sv ~/.dotfiles/git/.gitconfig.unix ~
 ln -sv ~/.dotfiles/git/.gitconfig.redgate ~
 ln -sv ~/.dotfiles/fish/config.fish ~/.config/fish/
+ln -sv ~/.dotfiles/starship/starship.toml ~/.config/
 
 # Restart Shell
 exec fish
