2043e8b81643eaeff25d8c6ad2e8ecccdb953ef9
David Eadie <david@eadie.net>
2022-07-31 09:54:13 +0100

    Add aliases to fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,25 @@
+# Get Brew apps on $PATH
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
+#Run tmux
 if status is-interactive
 and not set -q TMUX
     exec tmux
 end
 
+# Aliases
+if type -q exa; alias ls="exa"; end
+if type -q exa; alias la="exa -a"; end
+if type -q nvim; alias vim="nvim"; end
+alias cls="clear"
+alias k="kubectl"
+alias kc="kubectx"
+alias kn="kubens"
+alias d="docker"
+alias s="spawnctl"
+
+alias gs="git status"
+alias gd="git diff"
+
+# Start Starship
 starship init fish | source
