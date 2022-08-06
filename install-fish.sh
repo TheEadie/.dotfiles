dac86a4aa4856c2b973f51d9af56a1eddbfc0641
David Eadie <david@eadie.net>
2022-08-06 14:27:29 +0100

    Tmux setup

--- install-fish.sh
+++ install-fish.sh
@@ -9,6 +9,7 @@ rm ~/.gitconfig.unix
 rm ~/.gitconfig.redgate
 rm ~/.config/fish/config.fish
 rm ~/.config/starship.toml
+rm ~/.tmux.conf
 
 # Link files from the repo to the HOME dir
 ln -sv ~/.dotfiles/git/.gitconfig ~
@@ -16,6 +17,7 @@ ln -sv ~/.dotfiles/git/.gitconfig.unix ~
 ln -sv ~/.dotfiles/git/.gitconfig.redgate ~
 ln -sv ~/.dotfiles/fish/config.fish ~/.config/fish/
 ln -sv ~/.dotfiles/starship/starship.toml ~/.config/
+ln -sv ~/.dotfiles/tmux/.tmux.conf ~
 
 # Restart Shell
 exec fish
