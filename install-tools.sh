59dc112c18a5bdef2cc71b1c91b546895d0b420f
David Eadie <david@eadie.net>
2022-08-09 15:33:06 +0100

    Use Dracula theme for fish

--- install-tools.sh
+++ install-tools.sh
@@ -1,3 +1,4 @@
+# Install tools
 brew install exa
 brew install gh
 brew install bat
@@ -9,3 +10,6 @@ brew install git-delta
 # Install TMUX plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Install Fish plugins
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fisher install dracula/fish
