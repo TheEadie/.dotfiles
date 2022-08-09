f843129ff460262ba69ebcd1d4d712c45e1a8f14
David Eadie <david@eadie.net>
2022-08-09 14:55:59 +0100

    Use WSL git config in fish setup

--- install-fish.sh
+++ install-fish.sh
@@ -6,6 +6,7 @@ brew install starship
 # Clean up
 rm ~/.gitconfig
 rm ~/.gitconfig.unix
+rm ~/.gitconfig.wsl
 rm ~/.gitconfig.redgate
 rm ~/.config/fish/config.fish
 rm ~/.config/starship.toml
@@ -14,6 +15,7 @@ rm ~/.tmux.conf
 # Link files from the repo to the HOME dir
 ln -sv ~/.dotfiles/git/.gitconfig ~
 ln -sv ~/.dotfiles/git/.gitconfig.unix ~
+ln -sv ~/.dotfiles/git/.gitconfig.wsl ~
 ln -sv ~/.dotfiles/git/.gitconfig.redgate ~
 ln -sv ~/.dotfiles/fish/config.fish ~/.config/fish/
 ln -sv ~/.dotfiles/starship/starship.toml ~/.config/
